07f338f170ade0e880fa3cb5e08355b56ab1832e
Elliot Nou <woochachino@gmail.com>
2026-03-12 18:20:00 -0400

    run the jobs on a schedule

--- reminders/config/initializers/sidekiq.rb
+++ reminders/config/initializers/sidekiq.rb
@@ -4,6 +4,12 @@ REDIS_URL = ENV.fetch("REDIS_URL", "redis://localhost:6379/0")
 
 Sidekiq.configure_server do |config|
   config.redis = { url: REDIS_URL }
+
+  config.on(:startup) do
+    require "sidekiq/cron"
+    schedule = Rails.root.join("config/schedule.yml")
+    Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(schedule)) if File.exist?(schedule)
+  end
 end
 
 Sidekiq.configure_client do |config|
